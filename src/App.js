9a25c635661297fcda9539ab5438d14f07e61c62
jebitok-dev <sharonkosgei4@gmail.com>
2020-07-31 03:47:35 +0300

    start of monsters app

--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import './App.css';
 
 //using class gives access to state 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
+    }
+  }
+
+//JSX uses classname to dis
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
+      }
     </div>
     )
   }
