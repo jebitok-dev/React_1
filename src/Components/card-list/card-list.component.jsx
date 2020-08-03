5f96c15f7cd402907f415fcbb3f6a8c805f038c7
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-03 12:21:12 +0300

    styling for the cards

--- src/Components/card-list/card-list.component.jsx
+++ src/Components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ import './card-list.styles.css';
 export const CardList = (props) => {
     return <div className='card-list'>
         {
-        props.state.monsters.map(monster =>(
+        props.monsters.map(monster => (
         <Card key={monster.id} monster={monster} />
         ))}
     </div>;
