f47700bfebfce2ae23401137ebd6bf193e82b0af
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-21 12:43:20 +0300

    Updates

---
+++ precache-manifest.38eeff4589af4eddcd61706a4a651306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32542e821dcf308e880737d3276853e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a78dd780559c3f0a253b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f958a95134d979957d7",
+    "url": "/static/js/2.6932ae2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6932ae2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78dd780559c3f0a253b",
+    "url": "/static/js/main.58ac6f8a.chunk.js"
+  },
+  {
+    "revision": "80524adf7a680459ccea",
+    "url": "/static/js/runtime-main.f264c7c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
